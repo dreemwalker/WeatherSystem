7f993b79085078c5e150c0b923d43faa9f38cf3b
Radkevich Pavel <Dalatarion@gmail.com>
2019-10-02 21:42:35 +0300

    add config and backup

--- WpfClient/Models/JsonHttpClientSettings.cs
+++ WpfClient/Models/JsonHttpClientSettings.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Configuration;
 namespace WpfClient.Models
 {
     class JsonHttpClientSettings: IClientSettings
     {
-        public string baseUrl { get; set; } = "http://localhost:25477/Weather.svc/";
+        public string baseUrl { get; set; } = ConfigurationSettings.AppSettings.Get("ServiceBaseUrl");
         public string getCityMethodName { get; set; } = "GetCities";
         public string getWeatherMethodName { get; set; } = "GetWeather";
         public string getCityWeatherMethodName { get; set; } = "GetCityWeather";
